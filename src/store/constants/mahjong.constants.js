5884b118c860d21609f503892deaa8fad84b296c
serg <s.nazarchuck@gmail.com>
2019-05-23 03:38:56 +0300

    add reducers

--- src/store/constants/mahjong.constants.js
+++ src/store/constants/mahjong.constants.js
@@ -1,8 +1,4 @@
-export const CURRENT_PLAYER = 'CURRENT_PLAYER';
-export const CURRENT_STEP = 'CURRENT_STEP';
+export const AVAILABLE_CARDS = 'AVAILABLE_CARDS';
 export const CURRENT_AREA = 'CURRENT_AREA';
-export const ALL_STEPS = 'ALL_STEPS';
-export const SETS = 'SETS';
-export const WINS = 'WINS';
-export const NEW_SET = 'NEW_SET';
+export const BEST_RESULTS = 'BEST_RESULTS';
 export const NEW_GAME = 'NEW_GAME';
